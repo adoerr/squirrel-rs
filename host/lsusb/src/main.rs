5f3dc5c70099379e6d084eb12d9900e2ef2e22bd
adoerr <0xad@gmx.net>
2025-12-07 22:41:41 +0100

    bump host deps
    Signed-off-by: adoerr <0xad@gmx.net>

--- host/lsusb/src/main.rs
+++ host/lsusb/src/main.rs
@@ -1,7 +1,11 @@
-use anyhow::{anyhow, Result};
-use futures_lite::future::block_on;
+use std::time::Duration;
+
+use anyhow::{Result, anyhow};
 use log::info;
-use nusb::transfer::{ControlIn, ControlOut, ControlType, Recipient};
+use nusb::{
+    MaybeFuture,
+    transfer::{ControlIn, ControlOut, ControlType, Recipient},
+};
 
 const VENDOR_ID: u16 = 0x4242;
 const PRODUCT_ID: u16 = 0x4242;
@@ -9,7 +13,8 @@ const PRODUCT_ID: u16 = 0x4242;
 fn main() -> Result<()> {
     env_logger::init();
 
-    let dev_info = nusb::list_devices()?
+    let dev_info = nusb::list_devices()
+        .wait()?
         .find(|info| info.vendor_id() == VENDOR_ID && info.product_id() == PRODUCT_ID)
         .ok_or(anyhow!("Device not found"))?;
 
@@ -17,30 +22,38 @@ fn main() -> Result<()> {
     info!("Serial: {}", dev_info.serial_number().unwrap());
 
     // open device so we can interact with and claim interfaces
-    let dev = dev_info.open()?;
-    let iface = dev.claim_interface(0)?;
+    let dev = dev_info.open().wait()?;
+    let iface = dev.claim_interface(0).wait()?;
 
     // Send "hello world" to device
-    let res = block_on(iface.control_out(ControlOut {
-        control_type: ControlType::Vendor,
-        recipient: Recipient::Interface,
-        request: 100,
-        value: 200,
-        index: 0,
-        data: b"hello world",
-    }));
-
-    info!("{res:?}");
+    iface
+        .control_out(
+            ControlOut {
+                control_type: ControlType::Vendor,
+                recipient: Recipient::Interface,
+                request: 100,
+                value: 200,
+                index: 0,
+                data: b"hello world",
+            },
+            Duration::from_millis(100),
+        )
+        .wait()?;
 
     // Receive "hello" from device
-    let res = block_on(iface.control_in(ControlIn {
-        control_type: ControlType::Vendor,
-        recipient: Recipient::Interface,
-        request: 101,
-        value: 201,
-        index: 0,
-        length: 5,
-    }));
+    let res = iface
+        .control_in(
+            ControlIn {
+                control_type: ControlType::Vendor,
+                recipient: Recipient::Interface,
+                request: 101,
+                value: 201,
+                index: 0,
+                length: 5,
+            },
+            Duration::from_millis(100),
+        )
+        .wait()?;
 
     info!("{res:?}");
 
